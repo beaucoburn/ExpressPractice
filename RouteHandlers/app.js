28103eee79624ec8d1d9b503d58c4b1e999ba4bf
beaucoburn <douglas.coburn@gmail.com>
2024-09-04 11:20:20 -0600

    modified:   RouteHandlers/app.js

--- RouteHandlers/app.js
+++ RouteHandlers/app.js
@@ -32,6 +32,24 @@ const cb2 = function (req, res){
 
 app.get('/example/c', [cb0, cb1, cb2])
 
+//A combination of independent functions and arrays of functions can handle a route
+const cb3 - function (req, res, next){
+  console.log('CB3')
+  next()
+}
+
+const cb4 = function (req, res, next){
+  console.log('CB4')
+  next()
+}
+
+app.get('/example/d', [cb3, cb4], (req, res, next) => {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, (req, res) => {
+  res.send('Hello from D!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
