150a3894a467af05c6a56e5125474079c241fb3f
beaucoburn <douglas.coburn@gmail.com>
2024-09-04 11:29:47 -0600

    new file:   ExpressRouter/app.js

---
+++ ExpressRouter/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+//middleware that is specific to the router
+const timeLog = (req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+}
+router.use(timeLog)
+
+//define the home page route
+router.get('/', (req, res) => {
+  res.send('Welcome to the home page')
+})
+
+//define the about page route
+
